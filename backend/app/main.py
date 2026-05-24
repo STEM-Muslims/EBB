d1cbb3318b1ef6811a666289d248767bd7f56ce8
yasin <yasinakkhan04@gmail.com>
2026-05-24 23:59:00 +0100

    chore: modify allow origins to include ebb url

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://ebb.stemmuslims.com/",
+        "https://stemmuslims-ebb.netlify.app/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
